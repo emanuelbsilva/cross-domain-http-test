47642442ba99d91852d4415648c3959497263966
Emanuel Silva <emanuelbsilva@gmail.com>
2013-07-23 12:04:01 +0100

    Changed script html to request GET and POST

--- src/views/js/script.js
+++ src/views/js/script.js
@@ -1,6 +1,5 @@
-function callRemoteServer(address){
-	superagent
-		.get(address)
+function callRemoteServer(method, address){
+	superagent(method, address)
 		.end(function(res){
 			document.write(JSON.stringify(res.body));
 		});
